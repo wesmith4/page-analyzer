07f6771628a0f5a37f086a9914dba99de55b36da
Will Smith <56857148+wesmith4@users.noreply.github.com>
2022-11-27 09:12:10 -0500

    v1

---
+++ Page.py
@@ -0,0 +1,93 @@
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup, Tag, ResultSet
+from typing import TypedDict
+import json
+from wordcloud import WordCloud, STOPWORDS
+from spacy import load
+
+# Load the spaCy model
+nlp = load("en_core_web_sm")
+
+
+class PageMetaData(TypedDict):
+    title_tag: str
+    meta_description: str
+    h1: str
+
+
+@st.cache
+def get_text_from_url(url: str) -> str:
+    response = requests.get(url)
+    return response.text
+
+
+class Page:
+    def __init__(self, url: str):
+        self.url = url
+        self.text = get_text_from_url(url)
+        self.soup = BeautifulSoup(self.text, "html.parser")
+
+    def get_soup(self) -> BeautifulSoup:
+        return self.soup
+
+    def get_metadata(self) -> PageMetaData:
+        self.metadata: PageMetaData = {
+            "title_tag": self.soup.find("title").text,
+            "meta_description": self.soup.find(
+                "meta", {"name": "description"}
+            ).get("content"),
+            "h1": self.soup.find("h1").text,
+        }
+
+        return self.metadata
+
+    def get_headings(self, levels: list[str] = ["h2"]):
+        return {
+            level: list(map(
+                lambda tag: tag.text,
+                self.soup.find_all(level)
+            )) for level in levels
+        }
+
+    def get_schema_tags(self) -> ResultSet:
+        return self.soup.find_all(
+            "script",
+            {"type": "application/ld+json"}
+        )
+
+    def get_schema_objects(self) -> list[dict]:
+        schema_script_tags = self.soup.find_all(
+            "script",
+            {"type": "application/ld+json"}
+        )
+
+        schema_objects = list(map(
+            lambda tag: json.loads(tag.text),
+            schema_script_tags
+        ))
+
+        return schema_objects
+
+    def get_schema_object_from_tag(self, tag: Tag) -> dict:
+        return json.loads(tag.text)
+
+    def get_wordcloud(self):
+        paragraphs = list(map(lambda tag: tag.text, self.soup.find_all("p")))
+        text = " ".join(paragraphs)
+
+        stopwords = set(STOPWORDS)
+
+        cloud = WordCloud(stopwords=stopwords, max_words=40).generate(text)
+        return cloud
+
+    def get_entities(self, text: str):
+        doc = nlp(text)
+        return doc.ents
+
+    def get_concatenated_paragraphs(self) -> str:
+        paragraphs = list(map(
+            lambda tag: tag.text.strip(),
+            self.soup.find_all("p")
+        ))
+        return " ".join(paragraphs)
---
+++ app.py
@@ -0,0 +1,109 @@
+import streamlit as st
+from Page import Page
+from urllib.parse import urlparse
+from validators import url as is_url
+from matplotlib import pyplot as plt
+import pandas as pd
+
+st.set_page_config(page_title="Page Analyzer")
+st.title("Page Analyzer")
+
+# Get any query parameters, and set the input URL if present
+query_params = st.experimental_get_query_params()
+param_url: str = None
+for key, value in query_params.items():
+    if "url" in key:
+        param_url = value[0]
+
+input_url = st.text_input(
+    "Enter the URL of a page to analyze",
+    value=param_url if param_url else ""
+)
+
+# Validate the URL
+if input_url and is_url(input_url):
+    # Parse the URL
+    parsed_url = urlparse(input_url)
+
+    # Extract the properties of the parsed URL
+    domain = parsed_url.hostname
+    path = parsed_url.path
+    protocol = parsed_url.scheme
+
+    page = Page(input_url)
+
+    st.markdown(f"#### [Visit Page]({input_url})")
+
+    # Display the page's metadata
+    st.markdown("## Page Metadata")
+    metadata = page.get_metadata()
+    st.markdown(f"""
+    - Title Tag: `{metadata.get("title_tag")}`
+    - Meta Description: `{metadata.get("meta_description")}`
+    - H1: `{metadata.get("h1")}`
+    """)
+
+    # Display the page's h2s
+    st.markdown("## Page Headings")
+    heading_types = st.multiselect(
+        "Select heading levels to include",
+        options=["h1", "h2", "h3", "h4", "h5", "h6"],
+        default=["h2"]
+    )
+
+    headings_by_level = page.get_headings(heading_types)
+    # Display a bullet list of the page's h2s
+    num_expanders = 0
+    for level in sorted(list(headings_by_level.keys())):
+        num_expanders += 1
+
+        with st.expander(f"{level.upper()}s", expanded=num_expanders == 1):
+            st.markdown("\n".join(list(map(
+                lambda heading: f"- {heading}",
+                headings_by_level[level]
+            ))))
+
+    st.markdown("## Schema Markup")
+    # Display the page's schema markup
+    schema_tags = page.get_schema_tags()
+
+    for tag in schema_tags:
+        tag_id = tag.get("id")
+        schema_obj = page.get_schema_object_from_tag(tag)
+        with st.expander(f"{schema_obj.get('@type')} (tag id: {tag_id})"):
+            st.json(schema_obj)
+
+    st.markdown("## Word Cloud")
+    # Display a word cloud of the page's text
+    wordcloud = page.get_wordcloud()
+    fig = plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    st.pyplot(fig)
+
+    st.markdown("## Entities")
+    # Get a the list of the H2s on the page
+    h2s = headings_by_level.get("h2")
+
+    # Create a list of entities by their salience
+    concatenated_paragraphs = page.get_concatenated_paragraphs()
+    entities = page.get_entities(concatenated_paragraphs)
+    entity_freqs = {}
+    for entity in entities:
+        entity = entity.text.strip()
+        if entity in entity_freqs:
+            entity_freqs[entity] += 1
+        else:
+            entity_freqs[entity] = 1
+
+    # Display the entities in a table
+    st.markdown("### Entities by Frequency")
+    df = pd.DataFrame.from_dict(
+        entity_freqs,
+        orient="index",
+        columns=["Frequency"]
+    ).sort_values(by="Frequency", ascending=False)
+    st.dataframe(df)
+
+elif input_url and not is_url(input_url):
+    st.warning("Please enter a valid URL")
